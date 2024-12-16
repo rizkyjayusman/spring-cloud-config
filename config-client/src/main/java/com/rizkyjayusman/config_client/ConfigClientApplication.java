d34193d971560824a72355c68cc973baf1688e5b
Rizky Jayusman <rizky.jayusman96@gmail.com>
2024-12-16 07:18:41 +0700

    feat: connect config server from client

--- config-client/src/main/java/com/rizkyjayusman/config_client/ConfigClientApplication.java
+++ config-client/src/main/java/com/rizkyjayusman/config_client/ConfigClientApplication.java
@@ -1,8 +1,12 @@
 package com.rizkyjayusman.config_client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ConfigClientApplication {
 
@@ -10,4 +14,11 @@ public class ConfigClientApplication {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
 
+	@Value("${app.timeout}")
+	private String timeout;
+
+	@GetMapping
+	public void test() {
+		System.out.println(timeout);
+	}
 }
